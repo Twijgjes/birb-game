89298bfa9aac7a4600b1d0891901b8410bea8425
Roan <roanhageman@gmail.com>
2019-08-05 09:25:38 +0200

    First step to tree parameterisation

--- src/TreeGenerator.ts
+++ src/TreeGenerator.ts
@@ -17,25 +17,48 @@ interface BranchData {
   width: number;
   length: number;
   segments: number;
-  positions: FloatArray;
-  indices: IndicesArray;
 }
 
 interface BranchEndData extends BranchData {
   mesh: Mesh;
+  positions: number[];
+  indices: number[];
 }
 
 export function generateTreesInRadius(scene: Scene, center: Vector3, radius: number, amount: number) {
-  for (let i = 0; i < 50; i++) {
+  const treeOptions = {
+    startLength: 1,
+    startWidth: 1,
+    position: Vector3.Zero(),
+    direction: Vector3.Up(),
+    startSegments: 5,
+    recursions: 4,
+  };
+  for (let i = 0; i < amount; i++) {
       const distance = 10 + Math.random() * (radius - 10);
       const arc = Math.random() * Math.PI * 2;
-      generateTree(scene, 
-        center.add(new Vector3(0, 0, distance).rotateByQuaternionToRef(Quaternion.FromEulerAngles(0,arc,0), new Vector3())),
+      treeOptions.position = center
+        .add(new Vector3(0, 0, distance)
+          .rotateByQuaternionToRef(Quaternion
+            .FromEulerAngles(0,arc,0), new Vector3()
+      ));
+      generateTree(
+        scene,
+        treeOptions,
       );
   }
 }
 
-export function generateTree(scene: Scene, position: Vector3) {
+interface TreeOptions {
+  startLength: number;
+  startWidth: number;
+  position: Vector3;
+  direction: Vector3;
+  startSegments: number;
+  recursions: number;
+}
+
+export function generateTree(scene: Scene, treeOptions: TreeOptions) {
   const barkColor = Color3.FromHexString(COLORS.BROWN);
 
   const material = new StandardMaterial("terrainMaterial", scene);
@@ -44,29 +67,27 @@ export function generateTree(scene: Scene, position: Vector3) {
 
   const firstBranch = generateBranch(
     {
-      length: 1,
-      width: 1,
-      position,
+      length: treeOptions.startLength,
+      width: treeOptions.startWidth,
+      position: treeOptions.position,
       direction: Vector3.Up(),
-      segments: 5,
-      positions: [],
-      indices: [],
+      segments: treeOptions.startSegments,
     },
   );
-  const maxRecursions = 4;
+  const maxRecursions = treeOptions.recursions;
   const tree: TreeData = {
     branch: firstBranch,
     splits: [],
   }
-  prepMesh(firstBranch, barkColor, material);
-  const lastBranch = splitBranch(0, maxRecursions, tree, {material, barkColor});
-  // console.info(`Last branch ${lastBranch.positions.length}, ${lastBranch.indices.length}`)
-  // const mesh = prepMesh(lastBranch, barkColor, material);
-  // scene.addMesh(mesh);
-  // TODO: also get the stuff from the end
+  const meshes = [
+    ...splitBranch(0, maxRecursions, tree, {material, barkColor}), 
+    prepMesh(firstBranch, barkColor, material)
+  ];
+  const treeMesh = Mesh.MergeMeshes(meshes, true, false);
+  return treeMesh;
 }
 
-function splitBranch(recursions: number, max: number, treeData: TreeData, meshOptions: MeshOptions) /*: BranchEndData */{
+function splitBranch(recursions: number, max: number, treeData: TreeData, meshOptions: MeshOptions): Array<Mesh> {
   treeData.splits.push({
     branch: generateBranch(
       {
@@ -75,8 +96,6 @@ function splitBranch(recursions: number, max: number, treeData: TreeData, meshOp
         position: treeData.branch.position,
         direction: addRandomRotation(treeData.branch.direction, 1),
         segments: 5,
-        positions: [],// treeData.branch.positions,
-        indices: [],// treeData.branch.indices,
       },
     ),
     splits: []
@@ -89,40 +108,31 @@ function splitBranch(recursions: number, max: number, treeData: TreeData, meshOp
         position: treeData.branch.position,
         direction: addRandomRotation(treeData.branch.direction, 1),
         segments: 5,
-        positions: [],// treeData.branch.positions,
-        indices: [],// treeData.branch.indices,
       },
     ),
     splits: []
   });
   recursions++;
   
-  prepMesh(treeData.splits[0].branch, meshOptions.barkColor, meshOptions.material);
-  prepMesh(treeData.splits[1].branch, meshOptions.barkColor, meshOptions.material);
+  let meshes = [
+    prepMesh(treeData.splits[0].branch, meshOptions.barkColor, meshOptions.material),
+    prepMesh(treeData.splits[1].branch, meshOptions.barkColor, meshOptions.material),
+  ];
 
   if (recursions < max) {
-    const split1 = splitBranch(recursions, max, treeData.splits[0], meshOptions);
-    const split2 = splitBranch(recursions, max, treeData.splits[1], meshOptions);
-    // return {
-    //   ...split1,
-    //   positions: [...split1.positions, ...split2.positions],
-    //   indices: [...split1.indices, ...split2.indices],
-    // }
+    meshes = [
+      ...meshes,
+      ...splitBranch(recursions, max, treeData.splits[0], meshOptions),
+      ...splitBranch(recursions, max, treeData.splits[1], meshOptions),
+    ];
   } 
-  // else 
-  // {
-  //   return {
-  //     ...treeData.splits[0].branch,
-  //     positions: [...treeData.splits[0].branch.positions, ...treeData.splits[1].branch.positions],
-  //     indices: [...treeData.splits[0].branch.indices, ...treeData.splits[1].branch.indices],
-  //   };
-  // }
+  return meshes;
 }
 
 function generateBranch(
   options: BranchData,
 ): BranchEndData {
-  let { length, width, position, direction, segments, positions, indices } = options;
+  let { length, width, position, direction, segments} = options;
   // start with line segments
   const path = new Array<Vector3>();
   path.push(position);
@@ -149,8 +159,8 @@ function generateBranch(
   });
 
   const tube = extrTube;
-  positions = [...positions, ...tube.getVerticesData(BABYLON.VertexBuffer.PositionKind)];
-  indices = [...indices, ...tube.getIndices()];
+  const positions = [...tube.getVerticesData(BABYLON.VertexBuffer.PositionKind)];
+  const indices = [...tube.getIndices()];
   tube.dispose();
 
   const branchEndData = {
@@ -166,10 +176,10 @@ function generateBranch(
   return branchEndData;
 }
 
-function prepMesh(lastBranch: BranchEndData, barkColor: Color3, material: Material): Mesh {
+function prepMesh(branchEndData: BranchEndData, barkColor: Color3, material: Material): Mesh {
   const mesh: Mesh = new Mesh("tree");
-  const positions = lastBranch.positions;
-  const indices = lastBranch.indices;
+  const positions = branchEndData.positions;
+  const indices = branchEndData.indices;
   const normals = Array<number>();
   const colors = Array<number>();
   // tube.getVerticesData(BABYLON.VertexBuffer.ColorKind);
